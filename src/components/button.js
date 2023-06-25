548aca6d1c61abcf5c11546f1196e8aee71a124b
Driss KOHKOH <kohkoh.driss@gmail.com>
2023-06-25 17:38:52 +0200

    build app

--- src/components/button.js
+++ src/components/button.js
@@ -1,17 +1,31 @@
-import React, { Component } from "react";
-class Button extends Component {
-  render() {
-    return (
-      <div className={`column-${this.props.cols}`}>
-        <button
-          className="calc-button"
-          onClick={() => this.props.action(this.props.symbol)}
-        >
-          {this.props.symbol}
-        </button>
-      </div>
-    );
-  }
-}
+// import React, { Component } from "react";
+// class Button extends Component {
+//   render() {
+//     return (
+//       <div className={`column-${this.props.cols}`}>
+//         <button
+//           className="calc-button"
+//           onClick={() => this.props.action(this.props.symbol)}
+//         >
+//           {this.props.symbol}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Button;
+
+import React from "react";
+
+const Button = ({ cols, symbol, action }) => {
+  return (
+    <div className={`column-${cols}`}>
+      <button className="calc-button" onClick={() => action(symbol)}>
+        {symbol}
+      </button>
+    </div>
+  );
+};
 
 export default Button;
